9061dcbf365682c6433b7d04d8351f2255ffb073
Anand Dayalan <ananddayalan@Anands-MacBook-Pro.local>
2016-08-07 16:44:02 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# react-native-material-design-searchbar
+A React Native Material Design SearchBar
+
+<img src='https://i.imgsafe.org/7bc0b5f6fc.png' height='60'>
+
+<img src='https://i.imgsafe.org/7bc7f2a798.png' height='60'>
+
+# Usage
+
+Install the SearchBar from npm with `npm i -S react-native-material-design-searchbar`. Then, require it from your app's JavaScript files with `import SearchBar from 'react-native-material-design-searchbar'`.
+
+```js
+import React, { Component } from 'react';
+import SearchBar from 'react-native-material-design-searchbar';
+
+export default class ExampleComponent extends Component {
+  render() {
+    return (
+      <SearchBar
+        onSearchChange={() => console.log('On Focus')}
+        height={50}
+        onFocus={() => console.log('On Focus')}
+        onBlur={() => console.log('On Blur')}
+        placeholder={'Search...'}
+        autoCorrect={false}
+        padding={5}
+      />
+    );
+  }
+};
+
+```
+
+The React packager will include the SearchBar component in your app's JS package and make it available for your app to use.
+
+
+# Contributing
+
+Contributions are welcome.
+
+# LICENSE
+
+MIT
---
+++ SearchBar.js
@@ -0,0 +1,130 @@
+import React, { PropTypes } from 'react';
+import {
+  TextInput,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import dismissKeyboard from 'dismissKeyboard';
+
+const styles = StyleSheet.create({
+  searchBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    borderColor: '#b6b6b6',
+    borderStyle: 'solid',
+    borderWidth: 1,
+  },
+  searchBarInput: {
+    flex: 1,
+    fontWeight: 'normal',
+    color: '#212121',
+    backgroundColor: 'transparent',
+  },
+});
+
+class SearchBar extends React.Component {
+
+  static propTypes = {
+    height: PropTypes.number.isRequired,
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOnFocus: false,
+    };
+    this._onFocus = this._onFocus.bind(this);
+    this._onBlur = this._onBlur.bind(this);
+    this._onClose = this._onClose.bind(this);
+  }
+
+  _onClose() {
+    this._textInput.setNativeProps({ text: '' });
+    if (this.props.onClose) {
+      this.props.onClose();
+    }
+  }
+
+  _onFocus() {
+    this.setState({ isOnFocus: true });
+    if (this.props.onFocus) {
+      this.props.onFocus();
+    }
+  }
+
+  _onBlur() {
+    this.setState({ isOnFocus: false });
+    if (this.props.onBlur) {
+      this.props.onBlur();
+    }
+    dismissKeyboard();
+  }
+
+  render() {
+    const { height, autoCorrect, onSearchChange, placeholder, padding } = this.props;
+    const TouchableWrapperPadding = padding !== undefined ? padding : 5;
+    return (
+      <View
+        onStartShouldSetResponder={() => dismissKeyboard()}
+        style={{ padding: TouchableWrapperPadding }}
+      >
+        <View
+          style={
+            [styles.searchBar,
+              {
+                height: height + 10,
+                paddingLeft: height * 0.25,
+              },
+            ]
+          }
+        >
+          {this.state.isOnFocus ?
+            <TouchableOpacity onPress={() => dismissKeyboard()}>
+              <Icon
+                name={"md-arrow-back"} size={height * 0.5}
+                color="#737373"
+              />
+            </TouchableOpacity>
+          :
+            <Icon
+              name={"md-search"} size={height * 0.5}
+              color="#737373"
+            />
+          }
+          <TextInput
+            autoCorrect={autoCorrect === true}
+            ref={(c) => (this._textInput = c)}
+            onFocus={this._onFocus}
+            onBlur={this._onBlur}
+            onChange={onSearchChange !== undefined ? onSearchChange : undefined}
+            placeholder={placeholder !== undefined ? placeholder : 'Search...'}
+            placeholderTextColor="#bdbdbd"
+            style={
+              [styles.searchBarInput,
+                {
+                  paddingLeft: height * 0.5,
+                  fontSize: height * 0.4,
+                },
+              ]
+            }
+          />
+          {this.state.isOnFocus ?
+            <TouchableOpacity onPress={this._onClose}>
+              <Icon
+                style={{ paddingRight: height * 0.5 }}
+                name={"md-close"} size={height * 0.5}
+                color="#737373"
+              />
+            </TouchableOpacity>
+          : null
+          }
+        </View>
+      </View>
+    );
+  }
+}
+
+module.exports = SearchBar;
